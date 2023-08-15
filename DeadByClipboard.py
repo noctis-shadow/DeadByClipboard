3381359e67077b6a573e069a8991785732b560c7
Reiny Contanger <112801001+noctis-shadow@users.noreply.github.com>
2023-08-15 21:04:31 +0300

    Update

--- DeadByClipboard.py
+++ DeadByClipboard.py
@@ -1,31 +1,23 @@
 # -*- coding: utf-8 -*-
-from sys       import exit as shutdown
-from pynput    import keyboard
-from keyboard  import write
-from win32gui  import GetWindowText, GetForegroundWindow
-from pyperclip import paste
+from sys             import exit as shutdown
+from keyboard        import write
+from win32gui        import GetWindowText, GetForegroundWindow
+from pyperclip       import paste
+from pynput.keyboard import Key, HotKey, Listener
 
 
 def on_activate():
     try:
-        name = GetWindowText(GetForegroundWindow())
-
-        if "DeadByDaylight" in name:
+        if "DeadByDaylight" in GetWindowText(GetForegroundWindow()):
             write(paste())
     except:
         ...
 
-def for_canonical(key_event):
-    try:
-        return lambda key: key_event(listener.canonical(key)) if key != keyboard.Key.f8 else shutdown(0)
-    except:
-        ...
-
-hotkey = keyboard.HotKey(keyboard.HotKey.parse('<ctrl>+v'), on_activate)
+hotkey = HotKey(HotKey.parse('<ctrl>+v'), on_activate)
 
 if __name__ == "__main__":
-    with keyboard.Listener(
-        on_press   = for_canonical(hotkey.press),
-        on_release = for_canonical(hotkey.release)
+    with Listener(
+        on_press   = lambda key: hotkey.press(listener.canonical(key)) if key != Key.f8 else shutdown(0),
+        on_release = lambda key: hotkey.release(listener.canonical(key))
     ) as listener:
         listener.join()
